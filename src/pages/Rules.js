df417d5b764d428de5bd0dd002d3f90fc159c2d7
Danial Ahmad <108906119+iamdanialahmad@users.noreply.github.com>
2024-03-26 02:09:41 +0500

    Add new dependencies and implement rule management UI

--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import "./Rules.css";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import Navbar from "../components/Navbar";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { Dropdown } from "react-bootstrap";
 
 function RulesManagement({
   rules,
@@ -9,9 +12,23 @@ function RulesManagement({
   handleCommentChange,
   handleToggleStatus,
 }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const navigate = useNavigate();
   const handle_goback = () => {
     navigate("/home");
+  };
+
+  const [selectedFlags, setSelectedFlags] = useState([]);
+  const flags = ["C++", "Java", "ReactJS", "Spring Boot"];
+  const toggleLang = (option) => {
+    if (selectedFlags.includes(option)) {
+      setSelectedFlags(selectedFlags.filter((item) => item !== option));
+    } else {
+      setSelectedFlags([...selectedFlags, option]);
+    }
   };
 
   return (
@@ -21,36 +38,57 @@ function RulesManagement({
         <h1>Rules And Management</h1>
         <h2>Welcome Aboard</h2>
       </div>
-      <h3>Existing Rules List</h3>
-      <div className="content-area">
-        <table className="rules-table">
-          <tbody>
-            {rules.map((rule, index) => (
-              <tr key={index}>
-                <td>
-                  <button
-                    onClick={() => handleToggleStatus(index)}
-                    className={rule.status === "Active" ? "active" : "inactive"}
-                  >
-                    {rule.status}
-                  </button>
-                </td>
-                <td>{rule.name}</td>
-                <td>
-                  <textarea
-                    value={comments[index]}
-                    onChange={(event) => handleCommentChange(index, event)}
-                    placeholder="Add a comment"
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="table-section">
+        <div className="d-flex justify-content-end">
+          <button className="add-button" onClick={handleShow}>
+            Add
+          </button>
+        </div>
+        <div className="table"></div>
       </div>
       <button className="go-back" onClick={handle_goback}>
         Go Back
       </button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Rule</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="modal-body">
+            <div>
+              <div className="input-wrapper">
+                <label>Source IP</label>
+                <input type="text" />
+              </div>
+              <div className="input-wrapper">
+                <label>Destination IP</label>
+                <input type="text" />
+              </div>
+            </div>
+            <div>
+              <div className="input-wrapper">
+                <label>Flags</label>
+              </div>
+              <div className="input-wrapper">
+                <label>Payload Size</label>
+                <div className="d-flex gap-1">
+                  <input type="number" />
+                  {">"}
+                  <input type="number" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
